922cf12a753f7bbe0f50fff3b773446155de9ef9
sararun <sarosekvlad12@gmail.com>
2024-02-08 01:35:22 +0700

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserService } from './services/user/user.service';
+import { PrismaService } from './services/prisma/prisma.service';
+import { AuthController } from './controllers/auth/auth.controller';
+import { AuthModule } from './modules/auth/auth.module';
+import { AuthService } from './services/auth/auth.service';
+import { UserModule } from './modules/user/user.module';
+import { UserController } from './controllers/user/user.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule],
+  controllers: [AppController, AuthController, UserController],
+  providers: [AppService, UserService, PrismaService, AuthService],
 })
 export class AppModule {}
