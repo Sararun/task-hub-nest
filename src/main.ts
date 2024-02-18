7ddb9ba972332b3a0721d7f88117b51c68e24b9f
sararun <sarosekvlad12@gmail.com>
2024-02-19 00:23:12 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.use(cookieParser());
+  app.enableCors();
   await app.listen(3000);
 }
 
