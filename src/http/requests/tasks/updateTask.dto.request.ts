711a25cb21ceed0238b9ec95ea2990f6dd100099
sararun <sarosekvlad12@gmail.com>
2024-02-27 01:39:14 +0700

    add update task dto

---
+++ src/http/requests/tasks/updateTask.dto.request.ts
@@ -0,0 +1,48 @@
+import {
+  IsDate,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  Validate,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsDateBeforeNowValidator } from '../../../validators/isDateBeforeNow.validator';
+
+export class UpdateTaskDtoRequest {
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'The description of the task.',
+    example: 'Some kind of description',
+    required: false,
+    type: String,
+  })
+  readonly description: string | undefined;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(255)
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'The name of the task.',
+    example: 'Name of task',
+    required: true,
+    type: String,
+  })
+  readonly name: string | undefined;
+
+  @IsOptional()
+  @IsDate()
+  @Type(() => Date)
+  @Validate(IsDateBeforeNowValidator)
+  @ApiProperty({
+    description: 'Deadline date',
+    example: '2024-02-24T20:21:21.168Z',
+    required: false,
+    type: Date,
+  })
+  readonly deadline: Date | undefined;
+}
