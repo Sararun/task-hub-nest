eb4acc5698de9e18dfbe0170611c7fbc5d008f60
sararun <sarosekvlad12@gmail.com>
2024-04-01 22:05:34 +0700

    remove tag

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('task-hub-api')
     .setVersion('1.0')
-    .addTag('task-hub-api')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
